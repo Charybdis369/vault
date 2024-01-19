b9c6033abd2f1f6ee84d4acf2efefe96a5ec4026
richard <ricstewa41@cox.net>
2024-01-19 09:26:35 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//i created the variables that equal the combo numbers//
+
+const e = "3 + 7"
+
+const w = "40 - 60"
+
+const q = "3 * 9 + 2"
+
+console.log("10 - 40 - 39")
+
+//then i created a few alerts, these will display messages//
+//creating a dialog for the user displaying the vault codes//
+
+alert("You have been selected to open an important vault")
+
+alert("The combonation is")
+
+//these are the alerts using arithmetic to display the combonation//
+
+alert(3 + 7)
+alert(60 - 20)
+alert(3 * 9 + 2)
+
+
+
+
